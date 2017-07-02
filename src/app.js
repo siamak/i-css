1ab302907bb707942d66074fe9fefdd1afb5bc3f
Roman Orlovskiy <r.orlovskiy@smartplayer.org>
2017-07-02 13:21:51 +0300

    Move css and component in one file

--- src/app.js
+++ src/app.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import styles from 'app.css';
+import {addStyles} from 'i-css';
+
+const app = {
+    full: {
+        width: '100%',
+        height: '100%'
+    },
+    _global() {
+        return {
+            'html, body, #root': {
+                ...this.full,
+                padding: 0,
+                margin: 0
+            }
+        }
+    },
+    wrapper() {
+        return {
+            ...this.full,
+            backgroundColor: 'red'
+        }
+    }
+};
+addStyles(app);
 
 class App extends React.Component {
-    constructor(p_) {
-        super(p_);
-
-        this.state = {};
-    }
     render() {
         return (
-            <div className={styles.wrapper}>
+            <div className={app.wrapper}>
                 App
             </div>
         );
